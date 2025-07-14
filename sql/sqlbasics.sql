4571d7a9d12f8e564529009f4a1901654c9899a8
satyendragautam90 <satyendragautam901@gmail.com>
2025-07-14 16:54:05 +0530

    A dummy sql file add and sql basics file uncommented

--- sql/sqlbasics.sql
+++ sql/sqlbasics.sql
@@ -2,28 +2,28 @@ SELECT * FROM satyendragautam.student;
 
 -- select  StdName, StdAge from student where StdDept = 'CSE' and StdAge>='22';
 
--- select * from student, studentdepartment;
+select * from student, studentdepartment;
 
 -- Update
--- UPDATE student SET stdAge = stdAge + 1
+UPDATE student SET stdAge = stdAge + 1
 
 -- in
--- select StdName, StdDept from student where StdDept in('CSE', "IT");
+select StdName, StdDept from student where StdDept in('CSE', "IT");
 
 -- like
--- select StdName from student where StdName like '%a%';
+select StdName from student where StdName like '%a%';
  
 --  count
---  select count(*) as co from student ;
+ select count(*) as co from student ;
  
  -- sum
- -- select sum(StdId) as sum from student;
+ select sum(StdId) as sum from student;
  
---  SELECT NOW() AS current_datetime;
+ SELECT NOW() AS current_datetime;
 
--- between The BETWEEN operator is inclusive
+between The BETWEEN operator is inclusive
 
--- select StdName, StdAge from student where StdAge between '22' and '29';
+select StdName, StdAge from student where StdAge between '22' and '29';
 
 -- group by  clause in SQL is used to aggregate data across multiple rows and group the results based on one or more columns.
 
